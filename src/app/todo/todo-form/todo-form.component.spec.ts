e77207bcc1d3ac344638ea2128881a04c4c42a3c
ozcnii <ercount89@gmail.com>
2022-08-26 20:23:18 +0300

    add todo-form tests

--- src/app/todo/todo-form/todo-form.component.spec.ts
+++ src/app/todo/todo-form/todo-form.component.spec.ts
@@ -1,68 +1,73 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { ReactiveFormsModule } from '@angular/forms';
-// import { BrowserModule } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { addTodo } from '../todo.actions';
+import { Todo } from '../todo.models';
 
-// import { TodoFormComponent } from './todo-form.component';
+import { TodoFormComponent } from './todo-form.component';
 
-// describe('TodoFormComponent', () => {
-//   let component: TodoFormComponent;
-//   let fixture: ComponentFixture<TodoFormComponent>;
-//   let native: HTMLElement;
+describe('TodoFormComponent', () => {
+  let component: TodoFormComponent;
+  let fixture: ComponentFixture<TodoFormComponent>;
+  let native: HTMLElement;
+  let mockStore: MockStore;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [TodoFormComponent],
-//       imports: [BrowserModule, ReactiveFormsModule],
-//     }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TodoFormComponent],
+      imports: [BrowserModule, ReactiveFormsModule],
+      providers: [provideMockStore({ initialState: [] })],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(TodoFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//     native = fixture.nativeElement;
-//   });
+    fixture = TestBed.createComponent(TodoFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    mockStore = TestBed.inject(MockStore);
+    native = fixture.nativeElement;
+  });
 
-//   it('ok', () => {
-//     expect(1).toBe(1);
-//   });
+  it('should clear input field after submit', () => {
+    const text = 'some-text';
+    component.todoForm.setValue({ text });
+    expect(component.todoForm.value.text).toBe(text);
+    component.onSubmit();
+    expect(component.todoForm.value.text).toBe('');
+  });
 
-//   it('should clear input field after submit', () => {
-//     const text = 'some-text';
-//     component.todoForm.setValue({ text });
-//     expect(component.todoForm.value.text).toBe(text);
-//     component.onSubmit();
-//     expect(component.todoForm.value.text).toBe('');
-//   });
+  it('should call dispatch on submit', () => {
+    const text = 'some-text';
+    const dispatchSpy = spyOn(mockStore, 'dispatch');
 
-//   it('should emit value on submit', () => {
-//     const text = 'some-text';
+    component.todoForm.setValue({ text });
+    component.onSubmit();
 
-//     component.addTodoEvent.subscribe((v) => {
-//       expect(v.text).toBe(text);
-//     });
-//     component.todoForm.setValue({ text });
-//     component.onSubmit();
+    expect(dispatchSpy).toHaveBeenCalled();
+    expect(component.todoForm.value.text).toBe('');
+  });
 
-//     expect(component.todoForm.value.text).toBe('');
-//   });
+  it('button should disabled if input clean', () => {
+    const button = native.querySelector('button')!;
+    const dispatchSpy = spyOn(mockStore, 'dispatch');
+    spyOn(component, 'onSubmit');
 
-//   it('button should disabled if input clean', () => {
-//     const button = native.querySelector('button')!;
-//     button.click();
-//     spyOn(component, 'onSubmit');
-//     expect(component.onSubmit).not.toHaveBeenCalled();
-//   });
+    button.click();
 
-//   it('button should enabled if input not clean', () => {
-//     const text = 'some-text';
-//     const button = native.querySelector('button')!;
-//     const input = native.querySelector('input')!;
-//     spyOn(component, 'onSubmit');
+    expect(dispatchSpy).not.toHaveBeenCalled();
+    expect(component.onSubmit).not.toHaveBeenCalled();
+  });
 
-//     input.value = text;
-//     input.dispatchEvent(new Event('input'));
-//     fixture.detectChanges();
-//     button.click();
+  it('button should enabled if input not clean', () => {
+    const text = 'some-text';
+    const button = native.querySelector('button')!;
+    const input = native.querySelector('input')!;
+    spyOn(component, 'onSubmit');
 
-//     expect(component.onSubmit).toHaveBeenCalled();
-//   });
-// });
+    input.value = text;
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    button.click();
+
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+});
