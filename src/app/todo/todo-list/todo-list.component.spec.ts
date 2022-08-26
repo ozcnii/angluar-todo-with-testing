84bc1f784053e3152d6850621865d2b1cdf18537
ozcnii <ercount89@gmail.com>
2022-08-26 20:44:07 +0300

    add toto-list test

--- src/app/todo/todo-list/todo-list.component.spec.ts
+++ src/app/todo/todo-list/todo-list.component.spec.ts
@@ -1,57 +1,57 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { Todo } from '../todo.models';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Todo } from '../todo.models';
+import { TodoListComponent } from './todo-list.component';
 
-// import { TodoListComponent } from './todo-list.component';
+describe('TodoListComponent', () => {
+  let component: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+  let native: HTMLElement;
+  let mockStore: MockStore;
 
-// describe('TodoListComponent', () => {
-//   let component: TodoListComponent;
-//   let fixture: ComponentFixture<TodoListComponent>;
-//   let native: HTMLElement;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TodoListComponent],
+      providers: [provideMockStore({ initialState: [] })],
+    }).compileComponents();
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [TodoListComponent],
-//     }).compileComponents();
+    fixture = TestBed.createComponent(TodoListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    mockStore = TestBed.inject(MockStore);
+    native = fixture.nativeElement;
+  });
 
-//     fixture = TestBed.createComponent(TodoListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//     native = fixture.nativeElement;
-//   });
+  it('should render todos', () => {
+    const todos: Todo[] = [
+      {
+        id: 1,
+        isDone: true,
+        text: 'some-text',
+      },
+      {
+        id: 1,
+        isDone: true,
+        text: 'some-text',
+      },
+    ];
 
-//   it('ok', () => {
-//     expect(1).toBe(1);
-//   });
+    mockStore.setState({ todos });
 
-//   it('should render todos', () => {
-//     const todos: Todo[] = [
-//       {
-//         id: 1,
-//         isDone: true,
-//         text: 'some-text',
-//       },
-//       {
-//         id: 1,
-//         isDone: true,
-//         text: 'some-text',
-//       },
-//     ];
-//     component.todos = todos;
+    fixture.detectChanges();
+    const list = native.querySelectorAll('li');
+    expect(list.length).toBe(todos.length);
+  });
 
-//     fixture.detectChanges();
-//     const list = native.querySelectorAll('li');
-//     expect(list.length).toBe(todos.length);
-//   });
+  it('should`t render todos', () => {
+    const todos: Todo[] = [];
+    mockStore.setState({ todos });
 
-//   it('should`t render todos', () => {
-//     const todos: Todo[] = [];
-//     component.todos = todos;
+    fixture.detectChanges();
+    const list = native.querySelectorAll('li');
+    expect(list.length).toBe(todos.length);
 
-//     fixture.detectChanges();
-//     const list = native.querySelectorAll('li');
-//     expect(list.length).toBe(todos.length);
-
-//     const h3 = native.querySelector('h3')!;
-//     expect(h3.textContent).toContain('fount 0 todos');
-//   });
-// });
+    const h3 = native.querySelector('h3')!;
+    expect(h3.textContent).toContain('fount 0 todos');
+  });
+});
